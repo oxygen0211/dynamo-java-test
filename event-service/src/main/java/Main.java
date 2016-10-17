95e86e9d77afa97b9ceefe22b300e977653c2f01
j.engelhardt <jengelhardt211@gmail.com>
2016-10-17 14:53:17 +0200

    Initialize adapter once, poll less agressively

--- event-service/src/main/java/Main.java
+++ event-service/src/main/java/Main.java
@@ -6,13 +6,13 @@ import com.stoerk.cloud.event.service.data.adapter.DynamoDBAdapter;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
+		DynamoDBAdapter adapter = new DynamoDBAdapter();
 		while (true) {
 			long queryStart = System.currentTimeMillis();
-			DynamoDBAdapter adapter = new DynamoDBAdapter();
 			List<Event> events = adapter.retrieveEvents("103");
 			long queryDuration = System.currentTimeMillis() - queryStart;
 			System.out.println("Queried "+events.size()+" elements int "+queryDuration+"ms");
-			Thread.sleep(1000);
+			Thread.sleep(5000);
 		} 
 	}
 
